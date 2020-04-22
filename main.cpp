0103169eba99b4a1f2a4760919b870986f75c29e
ByDiDinDiy <dmitriy0bydi@gmail.com>
2020-04-22 23:00:20 +0400

    add 2 function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,41 @@
 #include <QCoreApplication>
 #include <QTextStream>
+#include <QDebug>
 
-QTextStream cin(stdin);
-QTextStream cout (stdout);
-
+int FindMaximum(QVector<int> array, int n);
+int SortArray(QVector <int> array, int maximum);
+#define MAX_SIZE = 5;
 
 int main(int argc, char *argv[])
 {
-    QVector <int> array;
 
-    cout << "hello world!";
+    QCoreApplication a(argc, argv);
+
+    QVector <int> array;
+    int32_t n = 0;
+
+    array = {3, 5, 8, 4, 11, 9};
+
+    n = 3;
+    int answer = FindMaximum(array, n);
     return a.exec();
+}
+
+int FindMaximum(QVector <int> array, int n)
+{
+    //TODO: empty
+    int maximum = INT_MAX;
+    for (int i = 0; i < MAX_SIZE; ++i)
+    {
+
+
+    }
+    return 0;
+}
+
+int SortArray(QVector <int> array, int maximum)
+{
+    int minimum = INT_MIN;
+    if ( )
+    return 0;
 }
