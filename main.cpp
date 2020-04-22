8f2da7049721d9cb9909520c2c6a57955b81b9e7
ByDiDinDiy <dmitriy0bydi@gmail.com>
2020-04-22 16:50:42 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QCoreApplication>
+#include <QTextStream>
+
+QTextStream cin(stdin);
+QTextStream cout (stdout);
+
+
+int main(int argc, char *argv[])
+{
+    QVector <int> array;
+
+    cout << "hello world!";
+    return a.exec();
+}
